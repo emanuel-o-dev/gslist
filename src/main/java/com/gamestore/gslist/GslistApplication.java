a7e5fcac5f4bed80e1f2f34fdc88edc6c4b38ca6
emanueloliveiraandrade <emanueloliveiraandrade@alunos.utfpr.edu.br>
2025-01-20 19:04:51 -0300

    project created

---
+++ src/main/java/com/gamestore/gslist/GslistApplication.java
@@ -0,0 +1,13 @@
+package com.gamestore.gslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GslistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GslistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gamestore/gslist/GslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamestore.gslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
