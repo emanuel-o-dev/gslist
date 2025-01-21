800165f7ba4f92f7c0484e9d732ab51fc287e8b3
emanueloliveiraandrade <emanueloliveiraandrade@alunos.utfpr.edu.br>
2025-01-21 16:27:01 -0300

    GET game by id

--- src/main/java/com/gamestore/gslist/services/GameService.java
+++ src/main/java/com/gamestore/gslist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.gamestore.gslist.dto.GameDto;
 import com.gamestore.gslist.dto.GameMinDTO;
 import com.gamestore.gslist.entities.Game;
 import com.gamestore.gslist.repositories.GameRepository;
@@ -15,10 +17,19 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true)
+	public GameDto findById(Long id){
+		Game result = gameRepository.findById(id).get();
+		GameDto dto = new GameDto(result);
+		return dto; 
+	}
+	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Game> result  = gameRepository.findAll();
 		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
-		return dto;
-		
+		return dto;		
 	}
+	
+	
 }
